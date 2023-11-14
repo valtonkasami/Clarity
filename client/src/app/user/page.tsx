45bd447ca0fbfb1e50732c23f3fc042e0b1e0a79
Valton Kasami <valton__@outlook.com>
2023-11-14 08:50:17 +0100

    Update page.tsx

--- client/src/app/user/page.tsx
+++ client/src/app/user/page.tsx
@@ -265,10 +265,10 @@ const User = () => {
     };
 
     
-const [imageLoaded, setImageLoaded] = useState(false);
+const [imageLoaded, setImageLoaded] = useState('hidden');
 
 const handleImageLoad = () => {
-  setImageLoaded(true);
+  setImageLoaded('');
 };
 
 
@@ -277,8 +277,8 @@ const handleImageLoad = () => {
         
         {user && <div className='flex xs:pr-14 mb-5  justify-center items-center xs:space-x-10 max-xs:space-x-5'>
             <div className='h-[200px] w-[200px] max-xs:h-[130px] max-xs:w-[130px] flex items-center justify-center border-[4px] border-[#444444] hover:border-red-600 overflow-hidden rounded-full'>
-                { user?.picturePath && imageLoaded && <img alt='img' className=' h-full w-full object-cover' onLoad={handleImageLoad} src={user?.picturePath}/>}
-                { !user?.picturePath && imageLoaded && <img alt='img' className=' h-full w-full object-cover' onLoad={handleImageLoad} src='cat.jpg'/>}
+                { user?.picturePath && <img alt='img' className={` ${imageLoaded} h-full w-full object-cover`} src={user?.picturePath} onLoad={handleImageLoad} />}
+                { !user?.picturePath && <img alt='img' className={` ${imageLoaded} h-full w-full object-cover`} src='cat.jpg' onLoad={handleImageLoad} />}
             </div>
 
             <div>
